0f9f3230c5c3ac2561433fe4fcc6070b5c4acc1a
Lachlan Deer <lachlan.deer@gmail.com>
2019-02-24 21:14:22 +0100

    load libraries conventionally

--- src/lib/knit_rmd.R
+++ src/lib/knit_rmd.R
@@ -10,6 +10,9 @@
 # Author: @lachlandeer
 #
 
+library(here)
+library(rmarkdown)
+
 # --- Command Line Unpack --- #
 args <- commandArgs(trailingOnly = TRUE)
 rmd_file = args[1]
@@ -17,15 +20,15 @@ out_file = basename(args[2])
 out_path = dirname(args[2])
 
 # --- Path where we want our pdf to end up --- #
-final_pdf = here::here(out_path, out_file)
+final_pdf = here(out_path, out_file)
 print(final_pdf)
 
 # --- Build file --- #
 ## knit R likes to build to its own directory, let's assemble what the
 ## name of the output will be
-tmp_pdf = here::here(dirname(rmd_file), out_file)
+tmp_pdf = here(dirname(rmd_file), out_file)
 ## knit
-rmarkdown::render(input = rmd_file, output_file = tmp_pdf, quiet=FALSE)
+render(input = rmd_file, output_file = tmp_pdf, quiet=FALSE)
 
 # --- Move build file --- #
 file.rename(tmp_pdf, final_pdf)
